1aebc9e8ce16f7072720667881d5186c8d3a0667
Piero Steinger <piero@steinger.eu>
2015-02-05 01:06:02 +0100

    configure bower, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+var bower_path = "./vendor/bower_components";
+var paths = {
+  'jquery'     : bower_path + "/jquery/dist",
+  'bootstrap'  : bower_path + "/bootstrap-sass-official/assets",
+  'fontawesome': bower_path + "/fontawesome"
+};
 
-elixir(function(mix) {
-    mix.less('app.less');
+elixir(function (mix) {
+  mix.sass("app.scss", "public/assets/css", {
+    includePaths: [
+      paths.bootstrap + '/stylesheets',
+      paths.fontawesome + '/scss'
+    ]
+  });
+
+  mix.scripts("app.js", "public/assets/js/app.js", "resources/assets/js");
+  mix.scripts([
+    paths.jquery + '/jquery.min.js',
+    paths.bootstrap + '/javascripts/bootstrap.min.js'
+  ], 'public/assets/js/dependencies.js', '.');
+
+  mix.copy(paths.fontawesome + '/fonts', 'public/assets/fonts');
 });
