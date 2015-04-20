b1cf33e9b3768e4b905535f24af79876913baddf
Kovah <mail@kovah.de>
2015-04-20 14:59:05 +0200

    Added first language strings for EN

---
+++ resources/lang/en/global.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'lc' => 'en', // 2-Letter Language Code
+    'ln' => 'English (US)', // Full Language Name
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoiceplane' => 'InvoicePlane',
+    'voucher' => 'Voucher',
+
+];
---
+++ resources/lang/en/setup.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Setup Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'ip_setup' => 'InvoicePlane Setup',
+    'next' => 'Next Step',
+    'ip_setup' => 'InvoicePlane Setup',
+    'ip_setup' => 'InvoicePlane Setup',
+
+    // Start and Language selection
+    'start.text' => 'Welcome to the InvoicePlane Setup.<br/>Please choose your language and click "Next" to continue.',
+    'choose_language' => 'Choose your language',
+
+    // Requirements Check
+    'requirements' => 'Requirements',
+    'requirements.text' => 'Welcome to the InvoicePlane Setup.<br/>Please choose your language and click "Next" to continue.',
+
+];
